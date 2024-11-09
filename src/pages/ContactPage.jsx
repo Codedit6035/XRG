a67e8a022e996e2339cc553e2ca0215e4f51b8c8
Elkotel Rabeb <elkotelrabeb111@gmail.com>
2024-11-09 15:46:29 +0100

    5 commit

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Contact from '../components/Contact/Contact'
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div>
+      <div className="  top-0 left-0 w-full z-20 bg-primary ">
+        <Navbar titreColor={'white'}/> {/* This is your existing Navbar component */}
+      </div>
+      <Contact/>
+    </div>
   )
 }
 
