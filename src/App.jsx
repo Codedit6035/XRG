3ce8e07338250b9dca7ea6e53371683748bfce62
Elkotel Rabeb <elkotelrabeb111@gmail.com>
2024-11-09 15:35:12 +0100

    3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
+import ServicesPage from './pages/ServicesPage';
+import AboutPage from './pages/AboutPage';
+import Footer from './components/Footer';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+
+
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
-    <div>App</div>
+    <div className=''>
+      <Routes>
+          <Route path="/"  element={<HomePage   /> } />
+          <Route path="/services" element={<ServicesPage  /> }/>
+          <Route path="/about"  element={<AboutPage /> } />
+          <Route path="/get in touch" element={<ContactPage  />  } />
+        </Routes>
+        <Footer/>
+
+
+    </div>
   )
 }
 
