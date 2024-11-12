24c220f4d6a088e58e20f31c3f22bb6b8e87cd54
Elkotel Rabeb <elkotelrabeb111@gmail.com>
2024-11-12 01:48:34 +0100

    31 commit

--- src/components/About/Ghazi/Expertise.jsx
+++ src/components/About/Ghazi/Expertise.jsx
@@ -2,7 +2,57 @@ import React from 'react'
 
 const  Expertise = () => {
   return (
-    <div> Expertise</div>
+    <div>
+      <div className='container flex flex-col gap-10 py-20'>
+        {/*Img*/}
+        <div>
+          <img src="" alt="" />
+        </div>
+        {/*Expert1*/}
+        <div className='flex gap-10'>
+          {/* title + Blue line */}
+          <div className='w-2/6'>
+          <h1 className='  text-xl text-primary mb-4'>
+            Leadership core competencies
+          </h1>
+          <div className="w-16 h-1 bg-blue-500 mb-4"></div>
+          </div>
+          {/* content */}
+          <div className='w-4/6'>
+            <ul className='flex flex-col gap-5'>
+              <li>Thought leadership</li>
+              <li>Strategic and operational sustainability management</li>
+              <li>Advocacy and Stakeholder Engagements</li>
+              <li>Global and cross-functional team leadership (Strategy, R&D, Product Innovation, Supply Chain, Marketing & Sales)</li>
+              <li>Product- and Portfolio Management</li>
+              <li>Global business development</li>
+              <li>Leading change and transformations</li>
+            </ul>
+          </div>
+        </div>
+        {/*Expert2*/}
+        <div className='flex gap-10'>
+          {/* title + Blue line */}
+          <div className='w-2/6'>
+          <h1 className='  text-xl text-primary mb-4'>
+          Expertise & Professional Qualifications
+          </h1>
+          <div className="w-16 h-1 bg-blue-500 mb-4"></div>
+          </div>
+          {/* content */}
+          <div className='w-4/6'>
+            <ul className='flex flex-col gap-5'>
+              <li>	Academic Lectures & Training: Designed and delivered academic courses focused on "Business & Society," with an emphasis on combining legal and management approaches</li>
+              <li>	Sustainability in Energy Technologies (power generation, transmission, and distribution)</li>
+              <li>	Product-level environmental impact assessment methodologies: Product Carbon Footprint (PCF) and Life Cycle Assessment (LCA) according to ISO 14040, ISO 14044, and ISO 14067, as well as sector-specific methodologies across industries like Metals, Chemicals, and Oil & Gas</li>
+              <li>	Greenhouse Gas (GHG) Scope 3 Accounting covering both upstream and downstream activities</li>
+              <li>	Sustainability Data Management Software (requirements engineering, selection, and implementation of sustainability data management software: supply chain management, product- and corporate-level sustainability reporting)</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
