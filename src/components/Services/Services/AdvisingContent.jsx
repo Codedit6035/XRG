bd261bd224c34cf1123b54f196206c921fd92971
Elkotel Rabeb <elkotelrabeb111@gmail.com>
2024-11-13 16:22:04 +0100

    47 commit

--- src/components/Services/Services/AdvisingContent.jsx
+++ src/components/Services/Services/AdvisingContent.jsx
@@ -4,15 +4,16 @@ import Adivising from "../../../assets/talking.jpg"
 const AdvisingContent = () => {
   return (
     <div>
-                {/* service1 */}
+                {/* part 1 */}
                 <div className='flex justify-center  my-20 gap-20 '>
                    {/* text*/}
-                   <div className='flex-1 text-primary font-raleway space-y-14'>
-                       <h1 className='text-6xl font-bold '>Advising</h1>
+                   <div className='flex-1 font-raleway space-y-14'>
+                       {/*advising + p*/}
+                       <h1 className='text-5xl  bg-shade1 text-white w-fit px-1'>Advising</h1>
                        <p className=' text-primary'>Empowering businesses to leverage their systemic strengths for long-term value and impact</p>
                        {/*corporations*/}
                        <div className='space-y-7'>
-                        <h1 className='text-4xl font-bold '>Corporations: </h1>
+                        <h1 className='text-2xl font-bold underline text-primary  '>Corporations: </h1>
                         <p>with a focus on resource-Intensive Corporations and Start-Ups in Energy & Utilities, Capital Goods Manufacturing, Materials, Chemicals, and Healthcare</p>
                         <ul className='list-disc space-y-3'>
                           <li>
@@ -28,10 +29,24 @@ const AdvisingContent = () => {
                             <span className='font-semibold'>Building a Compelling Business Case for Change– </span>crafting persuasive narratives to support the shift toward more sustainable strategies and business models.
                           </li>
                         </ul>
-                       </div>
+                       </div>                  
+                   </div>
+                   {/* image*/}
+                   <div  className='flex-1 '>
+                      <img src={Adivising } alt=""  className='h-[600px] w-fit'/>
+                   </div>
+                </div>   
+                {/* part 2 */}
+                <div className='flex justify-center  my-20 gap-20 '>
+                   {/* image*/}
+                   <div  className='flex-1 '>
+                      <img src={Adivising } alt=""  className='h-[600px] w-fit'/>
+                   </div>
+                   {/* text*/}
+                   <div className='flex-1 font-raleway space-y-14'>   
                        {/*Financial Services:*/}
                        <div className='space-y-7'>
-                        <h1 className='text-4xl font-bold '>Financial Services: </h1>
+                        <h1 className='text-2xl font-bold underline text-primary  '>Financial Services: </h1>
                         <ul className='list-disc space-y-3'>
                           <li>
                             <span className='font-semibold'>	Evaluating Environmental and Social Risks and Opportunities– </span>analyzing portfolio companies' business models to identify critical sustainability risks and growth opportunities.
@@ -43,10 +58,16 @@ const AdvisingContent = () => {
                             <span className='font-semibold'>	Defining and Guiding Transformation Needs–  </span>identifying essential transformation requirements for portfolio companies and supporting the implementation process
                           </li>
                         </ul>
-                       </div>
+                       </div>     
+                   </div>
+                </div>   
+                {/* part 3 */}
+                <div className='flex justify-center  my-20 gap-20 '>
+                   {/* text*/}
+                   <div className='flex-1 font-raleway space-y-14'>
                        {/*Policy and Standards Makers:*/}
                        <div className='space-y-10'>
-                        <h1 className='text-4xl font-bold '>Policy and Standards Makers:</h1>
+                        <h1 className='text-2xl font-bold underline text-primary '>Policy and Standards Makers:</h1>
                         <ul className='list-disc space-y-3'>
                           <li>
                             <span className='font-semibold'>Providing Data-Driven Insights to Policymakers–  </span>identifying critical levers to craft effective policies that drive meaningful change.
@@ -55,15 +76,18 @@ const AdvisingContent = () => {
                             <span className='font-semibold'>Advising Standardization Bodies on Sustainability Standards– </span> pinpointing gaps and recommending new standards at corporate and product levels to support an impact-driven economy.
                           </li>
                         </ul>
-                       </div>                     
+                       </div>         
                    </div>
                    {/* image*/}
-                   <div  className='flex-1'>
-                      <img src={Adivising } alt=""  className='h-[400px] w-auto'/>
+                   <div  className='flex-1 '>
+                      <img src={Adivising } alt=""  className='h-[600px] w-fit'/>
                    </div>
-                </div>           
+                </div>             
     </div>
   )
 }
 
 export default AdvisingContent
+
+
+  
