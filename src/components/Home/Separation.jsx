988526878d7a9e86e99625997c215d013826df71
Elkotel Rabeb <elkotelrabeb111@gmail.com>
2024-11-19 21:26:48 +0100

    76 commit

--- src/components/Home/Separation.jsx
+++ src/components/Home/Separation.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 
 const Separation = () => {
   return (
-    <div id="next-section" className='mt-[35px]'>
-        <div className='py-24  container text-center text-5xl font-Raleway text-primary'>            
-            <p className='text-2xl  '>“It is time to confront the uncomfortable truth: the ESG (Environmental, Social, and Governance) agenda, as we know it, is over. In the decade ahead, businesses must compete not only for market share but for the future itself. The rewards will be significant: long-term resilience, market leadership, and the ability to thrive in a world grounded in essential environmental and social foundations”. – Financial Times, September 2024</p>
+    <div id="next-section" className='  font-Raleway  mt-[35px] h-screen flex flex-col justify-center items-center gap-2 text-primary'  >
+                <h1 className=' text-4xl font-bold'> The End of Traditional ESG:</h1>    
+                <h1 className='text-6xl font-bold mb-5'>Competing for a Sustainable Future</h1>
+        <div className='text-justify relative bg-primary p-5 text-white font-semibold'>        
+          <p className='text-2xl mb-2 '>“It is time to confront the uncomfortable truth: the ESG (Environmental, Social, and Governance) agenda, as we know it, is over. In the decade ahead, businesses must compete not only for market share but for the future itself. The rewards will be significant: long-term resilience, market leadership, and the ability to thrive in a world grounded in essential environmental and social foundations”.</p>
+          <h1 className='absolute right-5 bottom-5 text-lg italic '> – Financial Times, <span className='text-base font-light'>September 2024</span></h1>
         </div>
     </div>
   )
